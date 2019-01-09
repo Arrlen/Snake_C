378ee7d838c0ccda1d72a3ccb846da8da30f07a5
Arrlen <rozwodniony1@gmail.com>
2019-01-09 22:28:16 +0100

    add basic instructions about the game and simple graphic logo "snake"

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,6 +1,37 @@
 #include "snake.h"
+#include "screen.h"
 
 CSnake::CSnake(CRect r, char _c /*=' '*/):
   CFramedWindow(r, _c)
 {
+   
+
+}
+void CSnake::paint(){
+    CFramedWindow::paint();
+    gotoyx(geom.topleft.y-1,geom.topleft.x);
+    printl(" Level: %d ",1);
+    gotoyx(geom.topleft.y+4,geom.topleft.x+4);
+    printl("h - toggle help");
+    gotoyx(geom.topleft.y+5,geom.topleft.x+4);
+    printl("p - pause/play");
+    gotoyx(geom.topleft.y+6,geom.topleft.x+4);
+    printl("r - restart");
+    gotoyx(geom.topleft.y+7,geom.topleft.x+4);
+    printl("use keys to move snake");
+    gotoyx(geom.topleft.y+8,geom.topleft.x+4);
+    printl("or window (in pause)");
+    gotoyx(geom.topleft.y+12,geom.topleft.x+4);
+    printl("****   *****  *****  *    *   *****");
+    gotoyx(geom.topleft.y+13,geom.topleft.x+4);
+    printl("*      *   *  *   *  *   *    *");
+    gotoyx(geom.topleft.y+14,geom.topleft.x+4);
+    printl("****   *   *  *****  ****     *****");
+    gotoyx(geom.topleft.y+15,geom.topleft.x+4);
+    printl("   *   *   *  *   *  *   *    *");
+    gotoyx(geom.topleft.y+16,geom.topleft.x+4);
+    printl("****   *   *  *   *  *    *   *****");
+    
+ 
+ 
 }
--- src/snake.h
+++ src/snake.h
@@ -2,12 +2,17 @@
 #define __SNAKE_H__
 
 #include "winsys.h"
-
+#include "screen.h"
+#include "cpoint.h"
+#include <vector>
 class CSnake:public CFramedWindow
 {
+private:
+  
+
 public:
   CSnake(CRect r, char _c = ' ');
-
+  void paint();
 };
 
 #endif
